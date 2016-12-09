41f2a560be1b08faacf0454eec56aef4d1ec39ef
Jannis Konrad <jannis.Konrad@h-brs.de>
2016-12-09 18:37:52 +0100

    test connect

--- cython/test.py
+++ cython/test.py
@@ -6,6 +6,7 @@ def read(x, y):
 
 def write(x, y):
   print("< write:", x, y)
+  ip, port = x
   return len(y)
 
 def pprint(x):
@@ -16,13 +17,31 @@ print("Log Level:", dtls.dtlsGetLogLevel())
 
 print("\nClient connect")
 
-d = dtls.DTLS(read=read, write=write)
+d = dtls.DTLS(read=read)
+e = dtls.DTLS(read=read)
+
+def writed(x, y):
+  addr, port = x
+  print('< write d', addr, port, y)
+  return len(y) if d.handleMessageAddr(addr, port, y, 0) == 0 else 0
+
+def writee(x, y):
+  addr, port = x
+  print('< write e', addr, port, y)
+  return len(y) if e.handleMessageAddr(addr, port, y, 0) == 0 else 0
+
+d.pycb['write'] = writee #lambda x,y: y if e.handleMessage(dtls.Session(x[0], x[1]), y, 0) == 0 else 0
+e.pycb['write'] = writed
+
+print(d.pycb)
+print(e.pycb)
+
+
 pprint("connect:")
 s = d.connect("::1", 20220)
-assert s == d.getSessionFromAddress("::1")
 
 pprint("try to send data")
-assert d.write(s, b"Test!") == 0 #peer not yet connected
+print(d.write(s, b"Test!")) # == 0 #peer not yet connected
 
 pprint("close connection")
 d.close(s)
