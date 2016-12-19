8a3b6005d4d443d2e413b6e748cb8a8c7203c60d
Jannis Konrad <jannis.Konrad@h-brs.de>
2016-12-19 17:22:55 +0100

    mixup server/client

--- cython/testMCSocketServer.py
+++ cython/testMCSocketServer.py
@@ -1,1 +1,6 @@
-import DTLSSocket; s = DTLSSocket.DTLSSocket(); s.joinMC("ff12::42", 2342, DTLSSocket.DTLS_CLIENT, b"secret"); s.sendmsg([b"Test1",], [], 0, ("ff12::42", 2342, 0, 0)); s.sendmsg([b"Test2",b"2"], [], 0, ("ff12::42", 2342, 0, 0));del s; print("Done")
+import DTLSSocket
+s = DTLSSocket.DTLSSocket()
+s.bind(("::", 2342, 0, 0))
+s.joinMC("ff12::42", 2342, DTLSSocket.DTLS_SERVER, b"secret")
+print(s.recvmsg(1200, cnt=50))
+print(s.recvmsg(1200, cnt=50))
